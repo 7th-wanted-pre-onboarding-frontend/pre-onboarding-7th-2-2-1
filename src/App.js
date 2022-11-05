2d5872b1a9feeaee81b29dc1987725fc76c76ffd
task11 <task11@naver.com>
2022-11-05 17:46:38 +0900

    feat: 프로젝트 초기 세팅 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function App() {
-  return <div>h,w</div>;
-}
+import AppRouter from './router';
 
-export default App;
+export default function App() {
+  return <AppRouter />;
+}
