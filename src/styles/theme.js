0a5e74a6a9248ac18950b550fcb18b35574ca068
task11 <task11@naver.com>
2022-11-05 21:59:00 +0900

    feat: theme 색상 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,7 @@
 const defaultTheme = {
   color: {
     default: '#3A474E',
+    white: '#FFFFFF',
     accent: '#586CF5',
     label: '#94A2AD'
   },
@@ -9,14 +10,15 @@ const defaultTheme = {
   },
   btnColor: {
     default: '#FFFFFF',
-    active: '#EDEFF1'
+    accent: '#586CF5'
   },
   bgColor: {
     default: '#F6F7F8'
   },
   border: {
     default: '#EDEFF1',
-    button: '#D1D8DC'
+    button: '#D1D8DC',
+    accent: '#586CF5'
   },
   tipColor: {
     default: '#E5F4FD'
