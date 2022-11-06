9166e92cf27dec91b11dc11f22a813497aa64e99
task11 <task11@naver.com>
2022-11-06 14:07:06 +0900

    feat: 광고 관리 필터 구현

---
+++ src/components/AdSetupFilter/AdSetupFilter.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import useDropdown from '../../utils/hooks/useDropdown';
+
+import Dropdown from '../Dropdown/Dropdown';
+import AdItemList from '../ProgressList/ProgressList';
+import Typography from '../Typography/Typography';
+
+import { PROGRESS } from '../../utils/constants/constList';
+
+import { StyledDropdownButton } from '../SidebarContainer/SidebarContainer.style';
+
+export default function AdSetupFilter() {
+  const { selectedItem, handleSelectBoxToggle, handleSelectItem, isToggled } =
+    useDropdown('전체 광고');
+
+  return (
+    <Dropdown size='sm'>
+      <StyledAdItemWrapper>
+        <Typography size='lg' variant='default'>
+          {selectedItem}
+        </Typography>
+      </StyledAdItemWrapper>
+      <Typography size='xlg' variant='default'>
+        <StyledDropdownButton onClick={handleSelectBoxToggle} />
+      </Typography>
+      {isToggled && (
+        <AdItemList list={PROGRESS} handleSelectItem={handleSelectItem} />
+      )}
+    </Dropdown>
+  );
+}
+
+const StyledAdItemWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+`;
